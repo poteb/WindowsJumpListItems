981bda15e51036b1620504218dd94f7f6d74b873
Paw <paw@pote.dk>
2021-09-10 10:53:35 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+DoSomeStuff();
+
+void DoSomeStuff()
+{
+    if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        return;
+
+    Console.WriteLine("Update jump list maximum number of items to: ");
+    var input = Console.ReadLine();
+    if (!int.TryParse(input, out _))
+    {
+        Console.WriteLine("Not an int.");
+        return;
+    }
+
+    var reg = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64).OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", true);
+    if (reg == null)
+    {
+        Console.WriteLine("Could not find key");
+        return;
+    }
+
+    reg.SetValue("JumpListItems_Maximum", input, RegistryValueKind.DWord);
+}
